26a99ecd9fb71f9c52c7dbec8e7e13c54e10e860
Adyatha G U <1rn19cs008.adyatha@gmail.com>
2022-11-06 16:41:43 +0530

    uploaded files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StoreProvider } from "./store";
+//import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StoreProvider>
+      <HelmetProvider>
+        <App />
+        {/* <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider> */}
+      </HelmetProvider>
+    </StoreProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <StoreProvider>
+//       <HelmetProvider>
+//         <App />
+//       </HelmetProvider>
+//     </StoreProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
